2d8748f80f10cbbac02950daaaf33ecfb7c435dc
RaameshB <geoplexofficial@gmail.com>
2021-05-21 15:57:56 -0500

    ExtraCommitThingIDK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomSamplePrograms/RobotConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomSamplePrograms/RobotConfiguration.java
@@ -1,22 +1,27 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.CustomSamplePrograms;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class RobotConfiguration {
+public class RobotConfiguration extends LinearOpMode {
     public DcMotor leftDrive = null;
     public DcMotor rightDrive = null;
 
     HardwareMap hwMap = null;
 
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 
     public void init(HardwareMap ahwmp) {
         hwMap = ahwmp;
 
         leftDrive = hwMap.get(DcMotor.class, "LM");
-        rightDrive = hwMap.get(DcMotor.class, "LM");
+        rightDrive = hwMap.get(DcMotor.class, "RM");
 
         leftDrive.setPower(0);
         rightDrive.setPower(0);
@@ -25,14 +30,33 @@ public class RobotConfiguration {
         rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    public void stop() {
+    public void stopMotors() {
         leftDrive.setPower(0);
         rightDrive.setPower(0);
     }
-
+    public boolean s = false;
     public void swapMotors() {
-        leftDrive = hwMap.get(DcMotor.class, "RM");
-        rightDrive = hwMap.get(DcMotor.class, "LM");
+        if (s) {
+            leftDrive = hwMap.get(DcMotor.class, "LM");
+            rightDrive = hwMap.get(DcMotor.class, "RM");
+            s = false;
+        } else {
+            leftDrive = hwMap.get(DcMotor.class, "RM");
+            rightDrive = hwMap.get(DcMotor.class, "LM");
+            s = true;
+        }
+
+    }
+
+    public boolean r;
+    public void directionSwap() {
+        if (r) {
+            forward();
+            r = false;
+        } else {
+            reverse();
+            r = true;
+        }
     }
 
     public void reverse() {
